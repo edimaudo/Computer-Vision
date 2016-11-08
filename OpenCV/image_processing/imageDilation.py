67c58cece2e73de6326e50399c1faa14670d4c6f
Edima Udo <edimaudo@gmail.com>
2016-11-08 18:10:09 -0500

    imageMorphology

--- OpenCV/image_processing/imageDilation.py
+++ OpenCV/image_processing/imageDilation.py
@@ -5,11 +5,11 @@ from matplotlib import pyplot as plt
 img = cv2.imread('j.png',0)
 kernel = np.ones((5,5),np.uint8)
 
-erosion = cv2.erode(img,kernel,iterations = 1)
+dilation = cv2.dilate(img,kernel,iterations = 1)
 
 plt.subplot(121),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(erosion),plt.title('Eroded')
+plt.subplot(122),plt.imshow(dilation),plt.title('Dilation')
 plt.xticks([]), plt.yticks([])
 plt.show()
 
--- OpenCV/image_processing/imageOpening.py
+++ OpenCV/image_processing/imageOpening.py
@@ -5,11 +5,11 @@ from matplotlib import pyplot as plt
 img = cv2.imread('j.png',0)
 kernel = np.ones((5,5),np.uint8)
 
-erosion = cv2.erode(img,kernel,iterations = 1)
+opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
 plt.subplot(121),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(erosion),plt.title('Eroded')
+plt.subplot(122),plt.imshow(opening),plt.title('Opening')
 plt.xticks([]), plt.yticks([])
 plt.show()
 
